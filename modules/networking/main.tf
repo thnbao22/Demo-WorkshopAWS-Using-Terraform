96376314edaa3c58881a2d53c2a8a1ad9a986679
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-11 23:14:42 +0700

    Create an IGW and a NAT GW

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "two_tier" {
 # So we have 4 subnets which is 2 public subnets and 2 private subnets
 
 ## Create Public Subnet 1 
-resource "aws_subnet" "two_tier_public_subnet" {
+resource "aws_subnet" "two_tier_public_subnet_1" {
   # (Required) The VPC ID.
   vpc_id                    = aws_vpc.two_tier.id
   # (Optional) AZ for the subnet.
@@ -27,7 +27,7 @@ resource "aws_subnet" "two_tier_public_subnet" {
 }
 
 ## Create Public Subnet 2 
-resource "aws_subnet" "two_tier_public_subnet" {
+resource "aws_subnet" "two_tier_public_subnet_2" {
   # (Required) The VPC ID.
   vpc_id                    = aws_vpc.two_tier.id
   # (Optional) AZ for the subnet.
@@ -42,7 +42,7 @@ resource "aws_subnet" "two_tier_public_subnet" {
 
 ## Create Private Subnet 1
 ### map_public_ip_on_launch is false means that the subnet is Private 
-resource "aws_subnet" "two_tier_private_subnet" {
+resource "aws_subnet" "two_tier_private_subnet_1" {
   vpc_id                    = aws_vpc.two_tier.id
   availability_zone         = "ap-southeast-1a"
   cidr_block                = "10.10.3.0/24"
@@ -53,7 +53,7 @@ resource "aws_subnet" "two_tier_private_subnet" {
 }
 
 ## Create Private Subnet 2
-resource "aws_subnet" "two_tier_private_subnet" {
+resource "aws_subnet" "two_tier_private_subnet_2" {
   vpc_id                    = aws_vpc.two_tier.id
   availability_zone         = "ap-southeast-1b"
   cidr_block                = "10.10.4.0/24"
@@ -63,3 +63,25 @@ resource "aws_subnet" "two_tier_private_subnet" {
   }
 }
 
+# Create an Internet Gateway
+resource "aws_internet_gateway" "two_tier_igw" {
+  vpc_id = aws_vpc.two_tier.id
+  tags = {
+    "Name" = "Internet Gateway"
+  }
+}
+
+# Create a NAT Gateway
+
+## Before we can create a NAT Gateway, we need to allocate an Elastic IP address.
+resource "aws_eip" "two_tier_nat" {
+  domain = "vpc"
+}
+# Provides a resource to create a VPC NAT Gateway.
+resource "aws_nat_gateway" "two_tier_nat" {
+  allocation_id     = aws_eip.two_tier_nat.id
+  subnet_id         = aws_subnet.two_tier_public_subnet_1.id
+  tags = {
+    "Name" = "NAT Gateway"
+  }
+}
