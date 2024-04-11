98ef6fe73bfff0f6a54d0220e7f449e4cf3a5d96
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-11 22:51:00 +0700

    Add the outputs of  ID of Public and Private Subnets

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -2,3 +2,11 @@
 output "vpc_id" {
   value = aws_vpc.two_tier.id
 }
+# the ID of Public Subnets
+output "public_subnets" {
+  value = aws_subnet.two_tier_public_subnet.*.id
+}
+# The ID of Private Subnets
+output "private_subnets" {
+  value = aws_subnet.two_tier_private_subnet.*.id
+}
