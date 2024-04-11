3ad54fd4963439a0429621b36a426382a53c41bf
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-11 22:58:04 +0700

    Modify the outputs of subnet

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -2,11 +2,19 @@
 output "vpc_id" {
   value = aws_vpc.two_tier.id
 }
-# the ID of Public Subnets
-output "public_subnets" {
-  value = aws_subnet.two_tier_public_subnet.*.id
+# the ID of Public Subnets 1
+output "public_subnets_1_id" {
+  value = aws_subnet.two_tier_private_subnet_1.id
 }
-# The ID of Private Subnets
-output "private_subnets" {
-  value = aws_subnet.two_tier_private_subnet.*.id
+# the ID of Public Subnets 2
+output "public_subnets_2_id" {
+  value = aws_subnet.two_tier_private_subnet_2.id
+}
+# The ID of Private Subnets 1
+output "private_subnets_1_id" {
+  value = aws_subnet.two_tier_private_subnet_1.id
+}
+# The ID of Private Subnets 2
+output "private_subnets_2_id" {
+  value = aws_subnet.two_tier_private_subnet_2.id
 }
