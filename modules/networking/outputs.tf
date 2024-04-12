31dd7edbe1e965c45e4aa768eb7c3b5be1bbdafe
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-13 01:44:37 +0700

    modify output

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -18,3 +18,19 @@ output "private_subnets_1_id" {
 output "private_subnets_2_id" {
   value = aws_subnet.two_tier_private_subnet_2.id
 }
+# the ID of route table public
+output "route_table_public_id" {
+  value = aws_route.two_tier_rt_public.id
+}
+# the ID of route table private
+output "route_table_private_id" {
+  value = aws_route.two_tier_rt_private.id
+}
+# the ID of Public Subnet SG
+output "public_subnet_sg_id" {
+  value = aws_security_group.two_tier_public_sg.id
+}
+# the ID of Private Subnet SG
+output "private_subnet_sg_id" {
+  value = aws_security_group.two_tier_private_sg.id
+}
