7f94f59a8ff5fa458b002a38e1c5634e555dbe3e
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-13 14:39:43 +0700

    Add module in the file main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,28 @@ provider "aws" {
 #  secret_key = 
 }
 
+locals {
+  instance_type = "t2.micro"
+  # use the Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume type
+  ami           = "ami-0fd77db8c27ba5cc5"
+}
+
+module "storage" {
+  source = "../modules/storage"
+}
+module "networking" {
+  source = "../modules/networking"
+}
+module "compute" {
+  source = "../modules/compute"
+  ami = local.ami
+  instance_type = local.instance_type
+  # We launch our Bastion Host on Public Subnet 2
+  bastion_subnet_id = module.networking.public_subnets_2_id
+  # We launch out EC2 Private on Private Subnet 1
+  private_subnet_id = module.networking.private_subnets_1_id
+  # SG for Public Subnet
+  bastion_sg = module.networking.public_subnet_sg_id
+  # SG fro Private Subnet
+  private_server_sg = module.networking.private_subnet_sg_id
+}
