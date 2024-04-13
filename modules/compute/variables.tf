f819ad9f53c09edbaa82bc4bce67a65c09c8ee39
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-13 12:00:14 +0700

    Define outputs for module compute

---
+++ modules/compute/variables.tf
@@ -0,0 +1,18 @@
+variable "ami" {
+  
+}
+variable "instance_type" {
+  
+}
+variable "bastion_sg" {
+  
+}
+variable "bastion_subnet_id" {
+  
+}
+variable "private_server_id" {
+  
+}
+variable "private_server_sg" {
+  
+}
