595df5b81d77e12fbdd95315f926ef55a8108b37
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-26 14:32:18 +0700

    Modify the attr bucket

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket        = "demo-bucket-1"
+  # If omitted, Terraform will assign a random, unique name. 
+  # bucket        = "demo-bucket-1"
   # Indicates that all objects in bucket must be emptied before deleting bucket 
   force_destroy = true  
 }
