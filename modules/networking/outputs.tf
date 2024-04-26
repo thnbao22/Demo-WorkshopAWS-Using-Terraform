9dfcdb7e2cc6e5893f692ad9db59f38c7cc1d4a7
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-26 19:55:26 +0700

    Fix the wrong output on public Subnet 1 and 2

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -4,11 +4,11 @@ output "vpc_id" {
 }
 # the ID of Public Subnets 1
 output "public_subnets_1_id" {
-  value = aws_subnet.two_tier_private_subnet_1.id
+  value = aws_subnet.two_tier_public_subnet_1.id
 }
 # the ID of Public Subnets 2
 output "public_subnets_2_id" {
-  value = aws_subnet.two_tier_private_subnet_2.id
+  value = aws_subnet.two_tier_public_subnet_2.id
 }
 # The ID of Private Subnets 1
 output "private_subnets_1_id" {
