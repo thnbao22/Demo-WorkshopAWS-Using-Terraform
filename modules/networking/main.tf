4dc99cbe79a03d5f0d5088f6fa5d849c60c38294
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-26 13:34:59 +0700

    Fix association 2 private subnet

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -137,7 +137,7 @@ resource "aws_route_table_association" "two_tier_rt_private_associate_1" {
 # Associate route table with private subnet 2
 resource "aws_route_table_association" "two_tier_rt_private_associate_2" {
   # Required The ID of the routing table to associate with.
-  route_table_id  = aws_route_table.two_tier_rt_public.id
+  route_table_id  = aws_route_table.two_tier_rt_private.id
   # Optional The subnet ID to create an association.
   subnet_id       = aws_subnet.two_tier_private_subnet_2.id
 }
